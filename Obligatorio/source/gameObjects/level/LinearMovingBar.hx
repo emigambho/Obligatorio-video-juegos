3ca1eac1bcfe8c740cc2c8435f89a39341ad3012
Emiliano Gamboa <emigambho@gmail.com>
2017-07-11 15:55:34 -0300

    Tailed

--- Obligatorio/source/gameObjects/level/LinearMovingBar.hx
+++ Obligatorio/source/gameObjects/level/LinearMovingBar.hx
@@ -9,6 +9,7 @@ import helpers.path.Linear;
 import helpers.path.PathWalker;
 import nape.geom.Vec2;
 import nape.phys.BodyType;
+import nape.phys.Material;
 
 /**
  * ...
@@ -23,9 +24,12 @@ class LinearMovingBar extends Bar
 	public function new(?X:Float = 0, ?Y:Float = 0, width:Int, height:Int, speed:Int, start:Point , end:Point)
 	{
 		super(X, Y,width,height);
-		this.setDrag(1, 1);
+		
 		myPath = new Linear(start, end);
 		pathWalker = PathWalker.fromSpeed(myPath, speed, PlayMode.Pong);
+		body.setShapeMaterials(new Material(0, 1, 2, 9000000, 0.001));
+		body.type = BodyType.DYNAMIC;
+		this.setDrag(1, 1);
 	}
 	
 	override public function update(elapsed:Float):Void
